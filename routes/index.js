b0d23f1903fcb470f8ca6dcc0ee21ebd5f5cd421
yukinj <yuki.coco7@gmail.com>
2019-10-18 11:05:33 -0700

    fix query 500 error

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
 var express = require('express');
+var url = require('url');
 var router = express.Router();
 
 router.use(function(req, res, next) {
   console.log(req.method, req.url);
+  console.log(__filename);
   next();
 });
-
+var make2, model,size;
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var query = req.query;
   res.render('index', { title: 'Express' });
 });
+
+router.get('/pageTwo', function(req, res, next) {
+  var mile = req.query.Mileage;
+  res.render('Page_2', { title: 'Express2' });
+  console.log("ths is mile",mile);
+  
+
+});
+
 
 module.exports = router;
